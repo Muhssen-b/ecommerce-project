176dd54765550d14eb89a083b93b5b29972e687c
Muhssen-b <muhssen.b@outlook.com>
2020-10-09 11:00:26 +1100

    Adding my files

---
+++ src/Homepage/Homepage.js
@@ -0,0 +1,52 @@
+import React from 'react';
+
+import './Homepage.scss'
+
+const Homepage = () => (
+    <div className = 'homepage'>
+        <div className="directory-menu">
+            <div className="menu-item">
+                <div className="content">
+                    <div className="title"> HATS </div>
+                        <span className="subtitle">SHOP NOW</span>
+                    </div>
+            </div>
+
+    
+        <div className="menu-item">
+            <div className="content">
+                <div className="title">JACKETS </div>
+                    <span className="subtitle"> </span>
+                </div>
+            </div>
+        </div>
+
+
+            <div className="menu-item">
+                <div className="content">
+                    <div className="title"> SNEAKERS  </div>
+                        <span className="subtitle"> SHOP NOW</span>
+                </div>
+            </div>
+        
+        <div className="menu-item">
+            <div className="content">
+                <div className="title"> WOMENS </div>
+                    <span className="subtitle"></span>
+            </div>
+        </div>
+    
+
+
+            <div className="menu-item">
+                <div className="content">
+                    <div className="title"> MENS </div>
+                        <span className="subtitle"></span>
+                </div>
+            </div>
+    </div>
+
+       
+)
+
+export default Homepage;
